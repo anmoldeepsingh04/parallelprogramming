1381969d29ea09cc855f29d23e322b4da92e13eb
Anmoldeep Singh <anmoldeep.singh.18003@iitgoa.ac.in>
2026-09-14 15:55:14 +0200

    first implementation with race conditions

--- firstExample.cpp
+++ firstExample.cpp
@@ -2,8 +2,23 @@
 #include <omp.h>
 
 int main(){
+    // #pragma omp parallel
+    // {
+    //     std::cout<<"Hello World!"<<std::endl;
+    // }
+
     #pragma omp parallel
     {
-        std::cout<<"Hello World!"<<std::endl;
+        int thread_id = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
+
+        // all threads compete for std::cout so we have jumbled output
+        std::cout<<"Hello World from "<< thread_id<<" of "<<num_threads<<std::endl;
+
+        // using this makes sure that this instruction is executed by one thread completely, only then the next thread begins
+        // #pragma omp critical
+        // {
+        //     std::cout<<"Hello World from "<< thread_id<<" of "<<num_threads<<std::endl;
+        // }
     }
 }
